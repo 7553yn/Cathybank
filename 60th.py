dc2b5cebbd0877431c7b02c44b27fe6690aefb8c
MaxWu <53346809+7553yn@users.noreply.github.com>
2023-10-21 09:17:17 +0800

    Update 60th.py

--- 60th.py
+++ 60th.py
@@ -1,7 +1,10 @@
 s = 'Hello welcome to Cathay 60th year anniversary'
 s = s.replace(' ','')
 s = s.upper()
+s = list(s)
+s.sort()
 
 for qstr in s:
+    set(s)
     qct = s.count(qstr) 
     print(qstr,qct)
